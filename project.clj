bcb83275f44e7ecf673d3d1ac69d03f9ffbd069b
ertugrulcetin <ertu.ctn@gmail.com>
2021-02-09 00:25:40 +0300

    update: project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject overload-fn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :description "Function overloading on type for Clojure"
+
+  :url "https://github.com/ertugrulcetin/overload-fn"
+
+  :author "Ertuğrul Çetin"
+
+  :license {:name "MIT License"
+            :url  "https://opensource.org/licenses/MIT"}
+
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}})
